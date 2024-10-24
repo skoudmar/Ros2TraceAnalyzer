55bbb62e55268047e8a1f73393a94157c5f322db
Martin Škoudlil <skoudmar@fel.cvut.cz>
2024-10-24 16:36:13 +0200

    Improve field
    + Added boolean
    + Added documentation
    + Query member by cstr without copying to CString
    + Generalize reading of static array to any int type

--- bt2-sys/src/field.rs
+++ bt2-sys/src/field.rs
@@ -1,20 +1,26 @@
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
+
 use derive_more::derive::Deref;
 
 use crate::raw_bindings::{
     bt_field, bt_field_array_borrow_element_field_by_index_const, bt_field_array_get_length,
-    bt_field_borrow_class_const, bt_field_class,
+    bt_field_bool_get_value, bt_field_borrow_class_const, bt_field_class,
     bt_field_class_structure_borrow_member_by_index_const,
     bt_field_class_structure_borrow_member_by_name_const,
     bt_field_class_structure_get_member_count, bt_field_class_structure_member,
     bt_field_class_structure_member_borrow_field_class_const,
     bt_field_class_structure_member_get_name, bt_field_class_type, bt_field_get_class_type,
-    bt_field_integer_unsigned_get_value, bt_field_string_get_length, bt_field_string_get_value,
+    bt_field_integer_signed_get_value, bt_field_integer_unsigned_get_value,
+    bt_field_string_get_length, bt_field_string_get_value,
     bt_field_structure_borrow_member_field_by_name_const,
 };
 
 #[repr(transparent)]
 pub struct BtFieldConst(*const bt_field);
+
+#[repr(transparent)]
+#[derive(Deref)]
+pub struct BtFieldBooleanConst(BtFieldConst);
 
 #[repr(transparent)]
 #[derive(Deref)]
@@ -38,6 +44,7 @@ pub struct BtFieldStructureConst(BtFieldConst);
 
 #[non_exhaustive]
 pub enum BtFieldType {
+    Boolean(BtFieldBooleanConst),
     UnsignedInteger(BtFieldUnsignedIntegerConst),
     SignedInteger(BtFieldSignedIntegerConst),
     String(BtFieldStringConst),
@@ -76,6 +83,23 @@ impl BtFieldConst {
         BtFieldClassType::from_field_class_type(class)
     }
 
+    /// Cast the field into a specific field type.
+    ///
+    /// # Returns
+    /// - `Ok(field)` if the field can be cast into the specified type.
+    /// - `Err((field, class))` if the field cannot be cast into the specified type.
+    ///     - `field` is the original field.
+    ///     - `class` is the `class_type` of the field.
+    ///
+    /// # Errors
+    /// The function will fail if the field type is not supported.
+    ///
+    /// # See also
+    /// - [`Self::into_uint()`] to cast the field into an unsigned integer.
+    /// - [`Self::into_int()`] to cast the field into a signed integer.
+    /// - [`Self::into_string()`] to cast the field into a string.
+    /// - [`Self::into_struct()`] to cast the field into a structure.
+    /// - [`Self::into_array()`] to cast the field into an array.
     pub fn cast(self) -> Result<BtFieldType, (Self, bt_field_class_type)> {
         let class = unsafe { bt_field_get_class_type(self.as_ptr()) };
         let Some(matched) = BtFieldClassType::from_field_class_type(class) else {
@@ -83,6 +107,7 @@ impl BtFieldConst {
         };
 
         Ok(match matched {
+            BtFieldClassType::Bool => BtFieldType::Boolean(BtFieldBooleanConst(self)),
             BtFieldClassType::UnsignedInteger => {
                 BtFieldType::UnsignedInteger(BtFieldUnsignedIntegerConst(self))
             }
@@ -96,6 +121,13 @@ impl BtFieldConst {
         })
     }
 
+    /// Cast the field into an unsigned integer.
+    ///
+    /// # Panics
+    /// If the field is not an unsigned integer.
+    ///
+    /// # See also
+    /// - [`Self::cast()`] to also obtain the field type.
     #[must_use]
     pub fn into_uint(self) -> BtFieldUnsignedIntegerConst {
         match self.cast() {
@@ -105,6 +137,13 @@ impl BtFieldConst {
         }
     }
 
+    /// Cast the field into a signed integer.
+    ///
+    /// # Panics
+    /// If the field is not a signed integer.
+    ///
+    /// # See also
+    /// - [`Self::cast()`] to also obtain the field type.
     #[must_use]
     pub fn into_int(self) -> BtFieldSignedIntegerConst {
         match self.cast() {
@@ -114,6 +153,13 @@ impl BtFieldConst {
         }
     }
 
+    /// Cast the field into a string.
+    ///
+    /// # Panics
+    /// If the field is not a string.
+    ///
+    /// # See also
+    /// - [`Self::cast()`] to also obtain the field type.
     #[must_use]
     pub fn into_string(self) -> BtFieldStringConst {
         match self.cast() {
@@ -123,6 +169,13 @@ impl BtFieldConst {
         }
     }
 
+    /// Cast the field into a structure.
+    ///
+    /// # Panics
+    /// If the field is not a structure.
+    ///
+    /// # See also
+    /// - [`Self::cast()`] to also obtain the field type.
     #[must_use]
     pub fn into_struct(self) -> BtFieldStructureConst {
         match self.cast() {
@@ -132,6 +185,13 @@ impl BtFieldConst {
         }
     }
 
+    /// Cast the field into an array.
+    ///
+    /// # Panics
+    /// If the field is not an array.
+    ///
+    /// # See also
+    /// - [`Self::cast()`] to also obtain the field type.
     #[must_use]
     pub fn into_array(self) -> BtFieldArrayConst {
         match self.cast() {
@@ -159,6 +219,7 @@ impl std::fmt::Debug for BtFieldConst {
         };
 
         match casted {
+            BtFieldType::Boolean(inner) => inner.fmt(f),
             BtFieldType::UnsignedInteger(inner) => inner.fmt(f),
             BtFieldType::SignedInteger(inner) => inner.fmt(f),
             BtFieldType::String(inner) => inner.fmt(f),
@@ -178,6 +239,7 @@ impl std::fmt::Display for BtFieldConst {
         };
 
         match casted {
+            BtFieldType::Boolean(inner) => inner.fmt(f),
             BtFieldType::UnsignedInteger(inner) => inner.fmt(f),
             BtFieldType::SignedInteger(inner) => inner.fmt(f),
             BtFieldType::String(inner) => inner.fmt(f),
@@ -192,6 +254,7 @@ impl std::ops::Deref for BtFieldType {
 
     fn deref(&self) -> &Self::Target {
         match self {
+            BtFieldType::Boolean(inner) => inner,
             BtFieldType::UnsignedInteger(inner) => inner,
             BtFieldType::SignedInteger(inner) => inner,
             BtFieldType::String(inner) => inner,
@@ -219,7 +282,19 @@ macro_rules! impl_debug_and_display_for_scalar_field {
     };
 }
 
+impl BtFieldBooleanConst {
+    /// Get the value of the boolean field.
+    #[must_use]
+    pub fn get_value(&self) -> bool {
+        0 != unsafe { bt_field_bool_get_value(self.as_ptr()) }
+    }
+}
+
+impl_debug_and_display_for_scalar_field!(BtFieldBooleanConst);
+
 impl BtFieldUnsignedIntegerConst {
+    /// Get the value of the unsigned integer field.
+    #[must_use]
     pub fn get_value(&self) -> u64 {
         unsafe { bt_field_integer_unsigned_get_value(self.as_ptr()) }
     }
@@ -228,20 +303,36 @@ impl BtFieldUnsignedIntegerConst {
 impl_debug_and_display_for_scalar_field!(BtFieldUnsignedIntegerConst);
 
 impl BtFieldSignedIntegerConst {
+    /// Get the value of the signed integer field.
+    #[must_use]
     pub fn get_value(&self) -> i64 {
-        unsafe { bt_field_integer_unsigned_get_value(self.as_ptr()) as i64 }
+        unsafe { bt_field_integer_signed_get_value(self.as_ptr()) }
     }
 }
 
 impl_debug_and_display_for_scalar_field!(BtFieldSignedIntegerConst);
 
 impl BtFieldStringConst {
+    /// Get the length of the string field.
+    #[must_use]
+    pub fn get_length(&self) -> u64 {
+        unsafe { bt_field_string_get_length(self.as_ptr()) }
+    }
+
+    /// Get the value of the string field.
+    ///
+    /// # Panics
+    /// - If the string length cannot fit into `usize`.
+    /// - If the string value cannot be converted to a `str`.
     #[must_use]
     pub fn get_value(&self) -> &str {
+        let length = self
+            .get_length()
+            .try_into()
+            .expect("String length is too large to convert to usize");
         unsafe {
-            let length = bt_field_string_get_length(self.as_ptr());
             let ptr = bt_field_string_get_value(self.as_ptr());
-            let bytes = std::slice::from_raw_parts(ptr.cast::<u8>(), length as usize);
+            let bytes = std::slice::from_raw_parts(ptr.cast::<u8>(), length);
             std::str::from_utf8(bytes)
                 .expect("BtFieldStringConst::get_value(): Failed to convert bytes to str")
         }
@@ -251,11 +342,16 @@ impl BtFieldStringConst {
 impl_debug_and_display_for_scalar_field!(BtFieldStringConst);
 
 impl BtFieldArrayConst {
+    /// Get the length of the array.
     #[must_use]
     pub fn get_length(&self) -> u64 {
         unsafe { bt_field_array_get_length(self.as_ptr()) }
     }
 
+    /// Get the value at the specified index.
+    ///
+    /// # Panics
+    /// - If the index is out of bounds.
     #[must_use]
     pub fn get_value(&self, index: u64) -> BtFieldConst {
         assert!(index < self.get_length());
@@ -268,25 +364,57 @@ impl BtFieldArrayConst {
         }
     }
 
+    /// Reads a static array of unsigned integers.
+    ///
+    /// # Panics
+    /// - If the length of the array does not match `N`.
+    /// - If the array contains a value that is not an unsigned integer.
+    /// - If any of the elements cannot be converted to `T`.
     #[must_use]
-    pub fn read_byte_array<const N: usize>(&self) -> [u8; N] {
+    pub fn read_static_unsigned_array<const N: usize, T>(&self) -> [T; N]
+    where
+        T: TryFrom<u64>,
+        <T as TryFrom<u64>>::Error: std::fmt::Debug,
+    {
         assert!(N == self.get_length().try_into().unwrap());
-        let mut array = [0; N];
 
-        for (i, dest) in array.iter_mut().enumerate() {
-            *dest = self
-                .get_value(i as u64)
+        let mut i = 0;
+        [0; N].map(|_| {
+            let elem = self
+                .get_value(i)
                 .into_uint()
                 .get_value()
                 .try_into()
                 .unwrap();
-        }
 
-        array
+            i += 1;
+            elem
+        })
+    }
+
+    /// Read a static array of signed integers.
+    ///
+    /// # Panics
+    /// - If the array length does not match the expected length `N`.
+    /// - If the array contains a value that is not a signed integer.
+    /// - If any of the elements cannot be converted to `T`.
+    #[must_use]
+    pub fn read_static_signed_array<const N: usize, T>(&self) -> [T; N]
+    where
+        T: TryFrom<i64>,
+        <T as TryFrom<i64>>::Error: std::fmt::Debug,
+    {
+        assert!(N == self.get_length().try_into().unwrap());
+
+        let mut i = 0;
+        [0; N].map(|_| {
+            let elem = self.get_value(i).into_int().get_value().try_into().unwrap();
+
+            i += 1;
+            elem
+        })
     }
 }
-
-impl_deref_for_field!(BtFieldArrayConst);
 
 impl std::fmt::Debug for BtFieldArrayConst {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -315,12 +443,48 @@ impl BtFieldStructureConst {
         BtFieldStructClassConst(self.0.get_class())
     }
 
+    /// Get the number of fields in the structure.
+    #[must_use]
+    #[inline]
+    pub fn get_field_count(&self) -> u64 {
+        self.get_class().get_member_count()
+    }
+
+    /// Get the field with the specified name.
+    ///
+    /// The field `name` is converted to a [`CString`] before being passed to the C API.
+    ///
+    /// # Returns
+    /// - `Some(field)` if the field exists.
+    /// - `None` if the field does not exist.
+    ///
+    /// # Panics
+    /// - If the field name cannot be converted to a [`CString`].
+    ///
+    /// # See also
+    /// - [`Self::get_field_by_name_cstr()`] to avoid converting the `name` to a [`CString`].
+    /// - [`Self::get_field_by_index()`] to get a field by its index.
     #[must_use]
     pub fn get_field_by_name(&self, name: &str) -> Option<BtFieldConst> {
-        let name = std::ffi::CString::new(name).expect(
+        let name = CString::new(name).expect(
             "BtFieldStructureConst::get_field_by_name(): Failed to convert name to CString",
         );
 
+        self.get_field_by_name_cstr(name.as_ref())
+    }
+
+    /// Get the field with the specified name.
+    ///
+    /// Note: This function is more efficient than [`Self::get_field_by_name()`] because it avoids converting the `name` to a [`CString`].
+    ///
+    /// # Returns
+    /// - `Some(field)` if the field exists.
+    /// - `None` if the field does not exist.
+    ///
+    /// # See also
+    /// - [`Self::get_field_by_index()`] to get a field by its index.
+    #[must_use]
+    pub fn get_field_by_name_cstr(&self, name: &CStr) -> Option<BtFieldConst> {
         unsafe {
             let field =
                 bt_field_structure_borrow_member_field_by_name_const(self.as_ptr(), name.as_ptr());
@@ -332,8 +496,14 @@ impl BtFieldStructureConst {
         }
     }
 
+    /// Get the field at the specified index.
+    ///
+    /// # Panics
+    /// - If the `index` is out of bounds.
     #[must_use]
     pub fn get_field_by_index(&self, index: u64) -> BtFieldConst {
+        assert!(index < self.get_field_count());
+
         unsafe {
             BtFieldConst::new_unchecked(bt_field_array_borrow_element_field_by_index_const(
                 self.as_ptr(),
@@ -392,7 +562,6 @@ impl BtFieldClassType {
                 Self::Array
             }
             _ => {
-                // TODO: Bool
                 // TODO: Float
                 // TODO: Enum
                 // TODO: Bit array
@@ -430,12 +599,42 @@ impl BtFieldStructClassConst {
         unsafe { bt_field_class_structure_get_member_count(self.as_ptr()) }
     }
 
+    /// Get the member with the specified `name`.
+    ///
+    /// The field `name` is converted to a [`CString`] before being passed to the C API.
+    ///
+    /// # Returns
+    /// - `Some(field)` if the field exists.
+    /// - `None` if the field does not exist.
+    ///
+    /// # Panics
+    /// - If the field name cannot be converted to a [`CString`].
+    ///
+    /// # See also
+    /// - [`Self::get_member_by_name_cstr()`] to avoid converting the `name` to a [`CString`].
+    /// - [`Self::get_member_by_index()`] to get a field by its index.
     #[must_use]
     pub fn get_member_by_name(&self, name: &str) -> Option<BtFieldStructMemberClassConst> {
-        let name = std::ffi::CString::new(name).expect(
+        let name = CString::new(name).expect(
             "BtFieldStructClassConst::get_field_by_name(): Failed to convert &str to CString",
         );
 
+        self.get_member_by_name_cstr(name.as_ref())
+    }
+
+    /// Get the member with the specified `name` as [`CStr`].
+    ///
+    /// Note: This function is more efficient than [`Self::get_member_by_name()`] because it avoids converting the `name` to a [`CString`].
+    ///
+    /// # Returns
+    /// - `Some(field)` if the field exists.
+    /// - `None` if the field does not exist.
+    ///
+    /// # See also
+    /// - [`Self::get_member_by_name()`] to convert the `name` to a [`CString`] before passing it to the C API.
+    /// - [`Self::get_member_by_index()`] to get a field by its index.
+    #[must_use]
+    pub fn get_member_by_name_cstr(&self, name: &CStr) -> Option<BtFieldStructMemberClassConst> {
         unsafe {
             let field =
                 bt_field_class_structure_borrow_member_by_name_const(self.as_ptr(), name.as_ptr());
@@ -447,8 +646,17 @@ impl BtFieldStructClassConst {
         }
     }
 
+    /// Get the member at the specified `index`.
+    ///
+    /// # Panics
+    /// - If the `index` is out of bounds.
+    ///
+    /// # See also
+    /// - [`Self::get_member_count()`] to get the number of members in the structure.
     #[must_use]
     pub fn get_member_by_index(&self, index: u64) -> BtFieldStructMemberClassConst {
+        assert!(index < self.get_member_count());
+
         unsafe {
             BtFieldStructMemberClassConst::new_unchecked(
                 bt_field_class_structure_borrow_member_by_index_const(self.as_ptr(), index),
@@ -464,14 +672,18 @@ impl BtFieldStructMemberClassConst {
         BtFieldStructMemberClassConst(field_class)
     }
 
+    /// Get the name of the structure member.
+    ///
+    /// # Panics
+    /// - If the name cannot be converted to a `str`.
     #[must_use]
     pub fn get_name(&self) -> &str {
         unsafe {
             let name = bt_field_class_structure_member_get_name(self.0);
             std::ffi::CStr::from_ptr(name)
-                .to_str()
-                .expect("Failed to convert CStr to str")
         }
+        .to_str()
+        .expect("Failed to convert CStr to str")
     }
 
     #[must_use]
