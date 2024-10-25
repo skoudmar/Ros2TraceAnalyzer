e25f62e59c22997789b3bbbd927ed2aaedc4289d
Martin Škoudlil <skoudmar@fel.cvut.cz>
2024-10-25 16:54:52 +0200

    Add ConstNonNull

--- bt2-sys/src/utils.rs
+++ bt2-sys/src/utils.rs
@@ -1,3 +1,5 @@
+use std::ptr::NonNull;
+
 #[macro_export]
 macro_rules! impl_deref {
     ($into:ty; for $($typ:ty),+) => {
@@ -11,4 +13,25 @@ macro_rules! impl_deref {
             }
         )+
     };
+}
+
+pub(crate) struct ConstNonNull<T>(NonNull<T>);
+
+impl<T> ConstNonNull<T> {
+    pub(crate) fn new(ptr: *const T) -> Option<Self> {
+        NonNull::new(ptr.cast_mut()).map(Self)
+    }
+
+    #[must_use]
+    pub(crate) fn as_ptr(&self) -> *const T {
+        self.0.as_ptr()
+    }
+}
+
+impl<T> TryFrom<*const T> for ConstNonNull<T> {
+    type Error = ();
+
+    fn try_from(ptr: *const T) -> Result<Self, Self::Error> {
+        Self::new(ptr).ok_or(())
+    }
 }
