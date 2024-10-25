d3ec9805d1a1e0d2f6357c0b901323cbb12baef0
Martin Škoudlil <skoudmar@fel.cvut.cz>
2024-10-25 16:52:57 +0200

    Suppress unused bindings from bt2

--- bt2-sys/src/raw_bindings.rs
+++ bt2-sys/src/raw_bindings.rs
@@ -1,3 +1,4 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
+#![expect(non_upper_case_globals)]
+#![expect(non_camel_case_types)]
+#![allow(unused)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
