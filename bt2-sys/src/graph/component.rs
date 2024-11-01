423f9f4c408345d9d3a4564e55563fcbdb25c9c3
Martin Škoudlil <skoudmar@fel.cvut.cz>
2024-11-01 15:23:55 +0100

    Implement info functions for Component

--- bt2-sys/src/graph/component.rs
+++ bt2-sys/src/graph/component.rs
@@ -1,18 +1,21 @@
 use std::ffi::CStr;
 use std::marker::PhantomData;
 use std::ptr::NonNull;
+use std::str::Utf8Error;
 
-use derive_more::derive::{Deref, DerefMut};
+use derive_more::derive::{Deref, DerefMut, From};
 
 use crate::query::{
     BtQueryError, BtQueryExecutor, SupportInfoParams, SupportInfoResult, SupportInfoResultError,
 };
 use crate::raw_bindings::{
     bt_component, bt_component_class, bt_component_class_filter,
-    bt_component_class_filter_as_component_class_const, bt_component_class_sink,
-    bt_component_class_sink_as_component_class_const, bt_component_class_source,
-    bt_component_class_source_as_component_class_const, bt_component_class_type,
-    bt_component_get_class_type, bt_component_get_ref, bt_component_put_ref,
+    bt_component_class_filter_as_component_class_const, bt_component_class_get_description,
+    bt_component_class_get_help, bt_component_class_get_name, bt_component_class_get_type,
+    bt_component_class_sink, bt_component_class_sink_as_component_class_const,
+    bt_component_class_source, bt_component_class_source_as_component_class_const,
+    bt_component_class_type, bt_component_get_class_type, bt_component_get_ref,
+    bt_component_put_ref,
 };
 use crate::utils::ConstNonNull;
 use crate::value::{BtValue, BtValueMap};
@@ -26,6 +29,18 @@ pub enum BtComponentType {
     Sink,
 }
 
+impl From<bt_component_class_type> for BtComponentType {
+    fn from(value: bt_component_class_type) -> Self {
+        match value {
+            bt_component_class_type::BT_COMPONENT_CLASS_TYPE_SOURCE => Self::Source,
+            bt_component_class_type::BT_COMPONENT_CLASS_TYPE_FILTER => Self::Filter,
+            bt_component_class_type::BT_COMPONENT_CLASS_TYPE_SINK => Self::Sink,
+            _ => unreachable!("Unknown component type: {}", value.0),
+        }
+    }
+}
+
+#[derive(From)]
 pub enum BtComponentCasted {
     Source(BtComponentSource),
     Filter(BtComponentFilter),
@@ -69,19 +84,11 @@ impl BtComponent {
 
     #[must_use]
     pub fn cast(self) -> BtComponentCasted {
-        let comp_type: bt_component_class_type =
-            unsafe { bt_component_get_class_type(self.as_ptr()) };
+        let comp_type = unsafe { bt_component_get_class_type(self.as_ptr()) }.into();
         match comp_type {
-            bt_component_class_type::BT_COMPONENT_CLASS_TYPE_SOURCE => {
-                BtComponentCasted::Source(BtComponentSource(self))
-            }
-            bt_component_class_type::BT_COMPONENT_CLASS_TYPE_FILTER => {
-                BtComponentCasted::Filter(BtComponentFilter(self))
-            }
-            bt_component_class_type::BT_COMPONENT_CLASS_TYPE_SINK => {
-                BtComponentCasted::Sink(BtComponentSink(self))
-            }
-            _ => unreachable!("Unknown component type: {}", comp_type.0),
+            BtComponentType::Source => BtComponentCasted::Source(BtComponentSource(self)),
+            BtComponentType::Filter => BtComponentCasted::Filter(BtComponentFilter(self)),
+            BtComponentType::Sink => BtComponentCasted::Sink(BtComponentSink(self)),
         }
     }
 }
@@ -102,6 +109,13 @@ impl Drop for BtComponent {
             bt_component_put_ref(self.as_ptr());
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, From)]
+pub enum BtComponentClassCastedConst<'a> {
+    Source(BtComponentClassSourceConst<'a>),
+    Filter(BtComponentClassFilterConst<'a>),
+    Sink(BtComponentClassSinkConst<'a>),
 }
 
 #[repr(transparent)]
@@ -134,8 +148,55 @@ impl<'a> BtComponentClassConst<'a> {
         Self(ConstNonNull::new_unchecked(ptr), PhantomData)
     }
 
+    pub fn name(&self) -> Result<&str, Utf8Error> {
+        unsafe {
+            let ptr = bt_component_class_get_name(self.as_ptr());
+            CStr::from_ptr(ptr).to_str()
+        }
+    }
+
+    pub fn description(&self) -> Option<Result<&str, Utf8Error>> {
+        unsafe {
+            let ptr = bt_component_class_get_description(self.as_ptr());
+            if ptr.is_null() {
+                None
+            } else {
+                Some(CStr::from_ptr(ptr).to_str())
+            }
+        }
+    }
+
+    pub fn help_text(&self) -> Option<Result<&str, Utf8Error>> {
+        unsafe {
+            let ptr = bt_component_class_get_help(self.as_ptr());
+            if ptr.is_null() {
+                None
+            } else {
+                Some(CStr::from_ptr(ptr).to_str())
+            }
+        }
+    }
+
     pub(crate) fn as_ptr(&self) -> *const bt_component_class {
         self.0.as_ptr()
+    }
+
+    pub fn get_type(&self) -> BtComponentType {
+        unsafe { bt_component_class_get_type(self.as_ptr()) }.into()
+    }
+
+    pub fn cast(self) -> BtComponentClassCastedConst<'a> {
+        match self.get_type() {
+            BtComponentType::Source => BtComponentClassCastedConst::Source(unsafe {
+                BtComponentClassSourceConst::new_unchecked(self.as_ptr().cast())
+            }),
+            BtComponentType::Filter => BtComponentClassCastedConst::Filter(unsafe {
+                BtComponentClassFilterConst::new_unchecked(self.as_ptr().cast())
+            }),
+            BtComponentType::Sink => BtComponentClassCastedConst::Sink(unsafe {
+                BtComponentClassSinkConst::new_unchecked(self.as_ptr().cast())
+            }),
+        }
     }
 
     #[must_use]
@@ -256,5 +317,38 @@ impl<'a> std::ops::Deref for BtComponentClassSinkConst<'a> {
         assert_eq!(self.as_ptr_upcast().cast(), self.as_ptr());
         // Safety: The upcasted pointer is the same as the original pointer and the types have the same layout.
         unsafe { &*std::ptr::from_ref(self).cast::<Self::Target>() }
+    }
+}
+
+impl<'a> std::fmt::Debug for BtComponentClassConst<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BtComponentClassConst")
+            .field("type", &self.get_type())
+            .field("name", &self.name())
+            .finish()
+    }
+}
+
+impl<'a> std::fmt::Debug for BtComponentClassSourceConst<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BtComponentClassSourceConst")
+            .field("name", &self.name())
+            .finish()
+    }
+}
+
+impl<'a> std::fmt::Debug for BtComponentClassFilterConst<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BtComponentClassFilterConst")
+            .field("name", &self.name())
+            .finish()
+    }
+}
+
+impl<'a> std::fmt::Debug for BtComponentClassSinkConst<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BtComponentClassSinkConst")
+            .field("name", &self.name())
+            .finish()
     }
 }
