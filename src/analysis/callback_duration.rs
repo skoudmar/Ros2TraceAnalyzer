99483b5b73ac41b1b7cd3ffa3ed366bb53150b0a
Martin Škoudlil <skoudmar@fel.cvut.cz>
2024-12-10 14:39:17 +0100

    Add inter-arrival time support to callback analysis

--- src/analysis/callback_duration.rs
+++ src/analysis/callback_duration.rs
@@ -1,18 +1,39 @@
-use serde::Serialize;
+use serde::{Serialize, Serializer};
 use std::collections::{HashMap, HashSet};
 use std::sync::{Arc, Mutex};
+use std::vec::Vec;
 
+use crate::args::ANALYSIS_CLI_ARGS;
+use crate::events_common::Context;
 use crate::model::display::{get_node_name_from_weak, DisplayCallbackSummary};
-use crate::model::{Callback, CallbackInstance};
+use crate::model::{Callback, CallbackInstance, Time};
 use crate::processed_events::{ros2, Event, FullEvent};
-use crate::statistics::calculate_min_max_avg;
+use crate::statistics::{Quantile, Sorted};
 use crate::utils::{DurationDisplayImprecise, WeakKnown};
 
 use super::{AnalysisOutput, ArcMutWrapper, EventAnalysis};
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
+pub struct ExecutionData {
+    #[serde(serialize_with = "serialize_time")]
+    pub start_time: Time,
+    pub duration: i64,
+    pub tid: u32,
+    pub cpuid: u32,
+}
+
+fn serialize_time<S>(time: &Time, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    s.serialize_i64(time.timestamp_nanos())
+}
+
+#[derive(Debug, Default)]
 pub struct CallbackDuration {
-    durations: HashMap<ArcMutWrapper<Callback>, Vec<i64>>,
+    execution_data: HashMap<ArcMutWrapper<Callback>, Vec<ExecutionData>>,
+    // durations: HashMap<ArcMutWrapper<Callback>, Vec<i64>>,
+    // execution_start_time: HashMap<ArcMutWrapper<Callback>, Vec<Time>>,
     started_callbacks: HashSet<ArcMutWrapper<CallbackInstance>>,
     not_ended_callbacks: Vec<ArcMutWrapper<CallbackInstance>>,
 }
@@ -29,20 +50,21 @@ pub struct Record {
     caller_param: String,
 
     durations: Vec<i64>,
+    inter_arrival_time: Vec<i64>,
 }
 
 #[derive(Debug, Clone)]
 pub struct RecordSummary {
     pub(crate) call_count: usize,
-    pub(crate) max_duration: i64,
-    pub(crate) min_duration: i64,
-    pub(crate) avg_duration: i64,
+    pub(crate) quantiles: Vec<(Quantile, i64)>,
 }
 
 impl CallbackDuration {
     pub fn new() -> Self {
         Self {
-            durations: HashMap::new(),
+            execution_data: HashMap::new(),
+            // durations: HashMap::new(),
+            // execution_start_time: HashMap::new(),
             started_callbacks: HashSet::new(),
             not_ended_callbacks: Vec::new(),
         }
@@ -62,17 +84,31 @@ impl CallbackDuration {
         self.started_callbacks.insert(callback.into());
     }
 
-    fn end_callback(&mut self, callback: Arc<Mutex<CallbackInstance>>) {
+    fn end_callback(&mut self, callback: Arc<Mutex<CallbackInstance>>, context: &Context) {
         let callback = callback.into();
         if self.started_callbacks.remove(&callback) {
             let callback_instance = callback.0.lock().unwrap();
             let duration = Self::calculate_duration(&callback_instance)
                 .expect("Duration should be known in callback_end");
 
-            self.durations
+            // self.durations
+            //     .entry(callback_instance.get_callback().into())
+            //     .or_default()
+            //     .push(duration);
+            // self.execution_start_time
+            //     .entry(callback_instance.get_callback().into())
+            //     .or_default()
+            //     .push(callback_instance.get_start_time());
+
+            self.execution_data
                 .entry(callback_instance.get_callback().into())
                 .or_default()
-                .push(duration);
+                .push(ExecutionData {
+                    start_time: callback_instance.get_start_time(),
+                    duration,
+                    tid: context.vtid(),
+                    cpuid: context.cpu_id(),
+                });
         } else {
             panic!("Callback {callback:?} was not started");
         }
@@ -102,27 +138,38 @@ impl CallbackDuration {
         &self,
         callback: &ArcMutWrapper<Callback>,
     ) -> Option<RecordSummary> {
-        let durations = self.durations.get(callback)?;
+        let exec_data = self.execution_data.get(callback)?;
         debug_assert!(
-            !durations.is_empty(),
-            "Callback should have at least one duration"
+            !exec_data.is_empty(),
+            "Callback should have at least one execution data. Otherwise, it should not be present in the map."
         );
 
-        let dur_len = durations.len();
-        let (min_duration, max_duration, avg_duration) = calculate_min_max_avg(durations)?;
+        let dur_len = exec_data.len();
+        let mut durations: Vec<i64> = exec_data.iter().map(|data| data.duration).collect();
+        durations.sort_unstable();
+        let durations_sorted = Sorted::from_sorted(durations).unwrap();
+
+        let duration_quantiles = ANALYSIS_CLI_ARGS
+            .get()
+            .expect("CLI args should be set")
+            .quantiles
+            .iter()
+            .map(|q| {
+                let duration = *durations_sorted.quantile(*q).expect("Should not be empty");
+                (*q, duration)
+            })
+            .collect::<Vec<_>>();
 
         Some(RecordSummary {
             call_count: dur_len,
-            max_duration,
-            min_duration,
-            avg_duration,
+            quantiles: duration_quantiles,
         })
     }
 
     pub fn get_records(&self) -> Vec<Record> {
-        self.durations
-            .keys()
-            .map(|callback_arc| {
+        self.execution_data
+            .iter()
+            .map(|(callback_arc, data)| {
                 let callback = callback_arc.0.lock().unwrap();
                 let node_name = callback.get_node().map_or(WeakKnown::Unknown, |node_weak| {
                     get_node_name_from_weak(&node_weak.get_weak())
@@ -135,7 +182,9 @@ impl CallbackDuration {
                     node: node_name.to_string(),
                     caller_type: callback_type.to_string(),
                     caller_param: callback_caller.to_string(),
-                    durations: self.durations[callback_arc].clone(),
+                    durations: data.iter().map(|data| data.duration).collect(),
+                    inter_arrival_time: Self::get_inter_arrival_time_inner(data)
+                        .unwrap_or_default(),
                 }
             })
             .collect()
@@ -143,7 +192,7 @@ impl CallbackDuration {
 
     pub(crate) fn print_stats(&self) {
         println!("Callback duration statistics:");
-        for (i, callback_arc) in self.durations.keys().enumerate() {
+        for (i, callback_arc) in self.execution_data.keys().enumerate() {
             let callback = callback_arc.0.lock().unwrap();
             let summary = self
                 .calculate_duration_summary(callback_arc)
@@ -152,37 +201,74 @@ impl CallbackDuration {
             println!("- [{i:4}] Callback {}:", DisplayCallbackSummary(&callback));
             println!("    Call count: {}", summary.call_count);
             if summary.call_count > 0 {
-                println!(
-                    "    Max duration: {}",
-                    DurationDisplayImprecise(summary.max_duration)
-                );
-                println!(
-                    "    Min duration: {}",
-                    DurationDisplayImprecise(summary.min_duration)
-                );
-                println!(
-                    "    Avg duration: {}",
-                    DurationDisplayImprecise(summary.avg_duration)
-                );
+                println!("    Duration quantiles:");
+                for (quantile, duration) in &summary.quantiles {
+                    println!(
+                        "        {quantile:5}: {duration_display}",
+                        duration_display = DurationDisplayImprecise(*duration),
+                    );
+                }
             }
         }
+    }
+
+    pub fn get_durations_for_callback(
+        &self,
+        callback: &ArcMutWrapper<Callback>,
+    ) -> Option<Vec<i64>> {
+        self.execution_data
+            .get(callback)
+            .map(|data| data.iter().map(|data| data.duration).collect::<Vec<_>>())
+    }
+
+    pub fn get_all_durations(&self) -> HashMap<ArcMutWrapper<Callback>, Vec<i64>> {
+        self.execution_data
+            .iter()
+            .map(|(k, v)| (k.clone(), v.iter().map(|data| data.duration).collect()))
+            .collect()
+    }
+
+    fn get_inter_arrival_time_inner(data: &[ExecutionData]) -> Option<Vec<i64>> {
+        if data.len() < 2 {
+            return None;
+        }
+
+        let mut inter_callback_time = Vec::with_capacity(data.len() - 1);
+
+        for w in data.windows(2) {
+            let (first_time, second_time) = (w[0].start_time, w[1].start_time);
+            assert!(second_time >= first_time);
+            let duration = second_time.timestamp_nanos() - first_time.timestamp_nanos();
+            inter_callback_time.push(duration);
+        }
+
+        Some(inter_callback_time)
+    }
+
+    pub fn get_inter_arrival_time(&self, callback: &ArcMutWrapper<Callback>) -> Option<Vec<i64>> {
+        let start_times = self.execution_data.get(callback)?;
+        Self::get_inter_arrival_time_inner(start_times)
+    }
+
+    pub fn get_execution_data(&self) -> &HashMap<ArcMutWrapper<Callback>, Vec<ExecutionData>> {
+        &self.execution_data
     }
 }
 
 impl EventAnalysis for CallbackDuration {
     fn initialize(&mut self) {
-        self.durations.clear();
+        self.execution_data.clear();
         self.started_callbacks.clear();
         self.not_ended_callbacks.clear();
     }
 
-    fn process_event(&mut self, event: &FullEvent) {
-        match &event.event {
+    fn process_event(&mut self, full_event: &FullEvent) {
+        match &full_event.event {
             Event::Ros2(ros2::Event::CallbackStart(event)) => {
                 self.start_callback(event.callback.clone());
             }
             Event::Ros2(ros2::Event::CallbackEnd(event)) => {
-                self.end_callback(event.callback.clone());
+                self.end_callback(event.callback.clone(), &full_event.context);
             }
 
             _ => {}
